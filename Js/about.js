5629e947f719f1e9acde9064bc719eb4d071c2e4
19ifte010 <19ifte010@gmail.com>
2022-01-08 17:46:29 +0530

    About Page Creation

---
+++ Js/about.js
@@ -0,0 +1,19 @@
+ScrollReveal().reveal('.slide-left',{
+    origin: "left",
+    distance: '150%',
+    opacity: 0.3,
+    delay: 300,
+    duration: 800,
+    easing: 'cubic-bezier(0,.47,.98,.98)',
+    usedelay: 'onload'
+});
+ScrollReveal().reveal('.slide-right',{
+    origin: "right",
+    distance: '10%',
+    opacity: null,
+    delay: 300,
+    duration: 500,
+    easing: 'cubic-bezier(0,.47,.98,.98)'
+});
+
+
